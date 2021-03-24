0a910d8ef4f092670bccd5b23a3a12529ab41859
RayHaha <g820324@gmail.com>
2021-03-24 00:38:18 -0400

    add component abilities, contact, experience, and projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AbilitiesComponent } from './abilities/abilities.component';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 import { EducationExperienceComponent } from './experience/education-experience.component';
+import { ExperienceComponent } from './experience/experience.component';
 import { WorkExperienceComponent } from './experience/work-experience.component';
 import { HeaderComponent } from './header/header.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { ProfileComponent } from './profile/profile.component';
     HeaderComponent,
     ProfileComponent,
     WorkExperienceComponent,
-    EducationExperienceComponent
+    EducationExperienceComponent,
+    AbilitiesComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
