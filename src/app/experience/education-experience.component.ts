cd81174e0022ed74a2ab100d999046b821f9ec2c
RayHaha <g820324@gmail.com>
2021-03-25 16:38:02 -0400

    experience

--- src/app/experience/education-experience.component.ts
+++ src/app/experience/education-experience.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-experience-education',
-    templateUrl: './education-experience.component.html'
+    templateUrl: './education-experience.component.html',
+    styleUrls: ['./education-experience.component.css']
 })
 export class EducationExperienceComponent{
 
