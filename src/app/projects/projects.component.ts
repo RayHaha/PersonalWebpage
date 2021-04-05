63fe0c936b2278aaa6125a386333d625768109e8
RayHaha <g820324@gmail.com>
2021-04-04 20:51:03 -0400

    projects and contact components

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,8 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-projects',
-    templateUrl: './projects.component.html'
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent{
-
+    openNewTab(link: string){
+        window.open(link, '_blank');
+    }
 }
